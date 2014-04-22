50a16b593ff6db5a215388846792cee621d6aecc
Brad Sawicki <git@bradsawicki.com>
2014-04-21 23:02:42 -0500

    Info page tpl clean up / Sass files clean up

--- sites/all/themes/jfro/templates/info.tpl.php
+++ sites/all/themes/jfro/templates/info.tpl.php
@@ -1,207 +1,187 @@
 <div class="grid info-grid">
-
-    <div class="row cf">
     
-	    <div class="c1 col">
-	        <img src="/sites/all/themes/jfro/assets/images/pic-jason.jpg" alt="" />
-	    </div><!-- c1 -->
-	    
-	    <div class="c2-c4 col" >
-	    
-	        <div class="row cf">
-	        
-	            <div class="c2 col">
-	                <h2 class="title">Contact:</h2>
-	                <p>Jason Frohlichstein<br />
-	                +1 (630) 546.8867<br />
-	                jason.frohlichstein@gmail.com</p>
-	            </div>
-	            
-	            <div class="c3-c4 col">
-	                <h2 class="title">About:</h2>
-	                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla placerat semper nisl, ac venenatis neque cursucs eget. Donec eleifend mi id sapien dapibus, in gravida augue tincidunt. Integer semper pharetra semper. Aliquam sed euismod dui. Vivamus ac velit vitae eros tempor viverra eget non nibh. Curabitur vitae sapien augue. Pellentesque molestie bibendum lacinia. Phasellus sit amet mollis sapien, et euismod sem.</p>
-	            </div>
-	        
-	        </div><!-- row -->
-
-	        <div class="row cf">
-
-	            <div class="c2 col">
-	                <h2 class="title">Found At:</h2>
-	                <ul>
-	                    <li><a href="">Leo Burnett DoD</a></li>
-                        <li><a href="">Instagram</a></li>
-                        <li><a href="">Dribbble</a></li>
-                        <li><a href="">Twitter</a></li>
-                        <li><a href="">LinkedIn</a></li>
-                        <li><a href="">Flickr</a></li>
-                        <li><a href="">Chicago Art department</a></li>
-	                </ul>
-	            </div>
-	            
-	            <div class="c3-c4 col">
-	                <h2 class="title">Services:</h2>
-	                <p>Branding & Identity Systems, Stationery, Icons/Pictograms/Symbols, Illustrations Print & Editorial Collarteral, Website & Application Design, Conceptualizing, Phography Direction, Environmental/Way-Finding Signage, Editorial Illustrations, Book Covers, Packaging, Installation & Exhibition Graphics, Posters, Restaurant Signage & Menu, and Screenprinting.</p>
-	            </div>
-	        
-	        </div><!-- row -->
-	    
-	    </div><!-- c2-c4 -->
+    <div class="col-1-4">
+        <img src="/sites/all/themes/jfro/assets/images/pic-jason.jpg" alt="" />
+    </div><!-- c1 -->
     
-    </div><!-- row -->
+    <div class="col-1-4 block-contact">
+        <h2 class="title">Contact:</h2>
+        <p>Jason Frohlichstein<br />
+        +1 (630) 546.8867<br />
+        jason.frohlichstein@gmail.com</p>
+    </div>
     
-    <div class="row cf">
+    <div class="col-1-2 block-about">
+        <h2 class="title">About:</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla placerat semper nisl, ac venenatis neque cursucs eget. Donec eleifend mi id sapien dapibus, in gravida augue tincidunt. Integer semper pharetra semper. Aliquam sed euismod dui. Vivamus ac velit vitae eros tempor viverra eget non nibh. Curabitur vitae sapien augue. Pellentesque molestie bibendum lacinia. Phasellus sit amet mollis sapien, et euismod sem.</p>
+    </div>
 
-	    <div class="c1 col instagram">
-            <?php if($instagram_photos): ?>
-            <h2 class="title">Recent Instagrams:</h2>
-            <div class="instagram-photos">
-              <?php foreach($instagram_photos as $photo_url): ?>
-              <div class="photo"><img src="<?php print $photo_url; ?>" /></div>
-              <?php endforeach; ?>  
-            </div>
-            <?php endif; ?>
-        </div><!-- c1 -->
-
-        <div class="c2-c4 col cf">
-        
-            <div class="c3-c4 col experience">
-                <h2 class="title">Experience:</h2>
-                <ul class="info-list">
-                    <li>
-                        <p class="meta-info">Designer, Sept. 2009 - Present</p>
-                        <h3>Leo Burnett, Department of Design</h3>
-                        <p>Contribute and/or lead creativity for multiple international brands, from concept to creation for campaigns spanning print, digital, and environmental.</p>
-                    </li>
-                    <li>
-                        <p class="meta-info">Designer, Sept. 2009 - Present</p>
-                        <h3>Leo Burnett, Department of Design</h3>
-                        <p>Contribute and/or lead creativity for multiple international brands, from concept to creation for campaigns spanning print, digital, and environmental.</p>
-                    </li>
-                    <li>
-                        <p class="meta-info">Designer, Sept. 2009 - Present</p>
-                        <h3>Leo Burnett, Department of Design</h3>
-                        <p>Contribute and/or lead creativity for multiple international brands, from concept to creation for campaigns spanning print, digital, and environmental.</p>
-                    </li>
-                    <li>
-                        <p class="meta-info">Designer, Sept. 2009 - Present</p>
-                        <h3>Leo Burnett, Department of Design</h3>
-                        <p>Contribute and/or lead creativity for multiple international brands, from concept to creation for campaigns spanning print, digital, and environmental.</p>
-                    </li>
-                </ul>
-            </div>
-
-            <div class="c2 col exhibition">
-                <h2 class="title">Exhibitions:</h2>
-                <ul class="info-list">
-                    <li>
-                        <h3>Estate Sale</h3>
-                        <p>Curator for an exhibition featuring the work of artists Ryan Maher & Ariel Diaz.</p>
-                        <p class="meta-info">Chicago Art Department, Chicago, IL - 2013</p>
-                    </li>
-                    <li>
-                        <h3>Estate Sale</h3>
-                        <p>Curator for an exhibition featuring the work of artists Ryan Maher & Ariel Diaz.</p>
-                        <p class="meta-info">Chicago Art Department, Chicago, IL - 2013</p>
-                    </li>
-                    <li>
-                        <h3>Estate Sale</h3>
-                        <p>Curator for an exhibition featuring the work of artists Ryan Maher & Ariel Diaz.</p>
-                        <p class="meta-info">Chicago Art Department, Chicago, IL - 2013</p>
-                    </li>
-                    <li>
-                        <h3>Estate Sale</h3>
-                        <p>Curator for an exhibition featuring the work of artists Ryan Maher & Ariel Diaz.</p>
-                        <p class="meta-info">Chicago Art Department, Chicago, IL - 2013</p>
-                    </li>
-                    <li>
-                        <h3>Estate Sale</h3>
-                        <p>Curator for an exhibition featuring the work of artists Ryan Maher & Ariel Diaz.</p>
-                        <p class="meta-info">Chicago Art Department, Chicago, IL - 2013</p>
-                    </li>
-                    <li>
-                        <h3>Estate Sale</h3>
-                        <p>Curator for an exhibition featuring the work of artists Ryan Maher & Ariel Diaz.</p>
-                        <p class="meta-info">Chicago Art Department, Chicago, IL - 2013</p>
-                    </li>
-                    <li>
-                        <h3>Estate Sale</h3>
-                        <p>Curator for an exhibition featuring the work of artists Ryan Maher & Ariel Diaz.</p>
-                        <p class="meta-info">Chicago Art Department, Chicago, IL - 2013</p>
-                    </li>
-                    <li>
-                        <h3>Estate Sale</h3>
-                        <p>Curator for an exhibition featuring the work of artists Ryan Maher & Ariel Diaz.</p>
-                        <p class="meta-info">Chicago Art Department, Chicago, IL - 2013</p>
-                    </li>
-                    <li>
-                        <h3>Estate Sale</h3>
-                        <p>Curator for an exhibition featuring the work of artists Ryan Maher & Ariel Diaz.</p>
-                        <p class="meta-info">Chicago Art Department, Chicago, IL - 2013</p>
-                    </li>
-                </ul>
-            </div>
-            
-            <div class="c3 col">
-                <h2 class="title">Honorable Mentions:</h2>
-                <ul class="info-list">
-                    <li>
-                        <h3>FPO Award</h3>
-                        <p>The Greatest Story Ever Folded</p>
-                        <p class="meta-info">Rank: Salvage, 2011-2012</p>
-                    </li>
-                    <li>
-                        <h3>FPO Award</h3>
-                        <p>The Greatest Story Ever Folded</p>
-                        <p class="meta-info">Rank: Salvage, 2011-2012</p>
-                    </li>
-                    <li>
-                        <h3>FPO Award</h3>
-                        <p>The Greatest Story Ever Folded</p>
-                        <p class="meta-info">Rank: Salvage, 2011-2012</p>
-                    </li>
-                    <li>
-                        <h3>FPO Award</h3>
-                        <p>The Greatest Story Ever Folded</p>
-                        <p class="meta-info">Rank: Salvage, 2011-2012</p>
-                    </li>
-                    <li>
-                        <h3>FPO Award</h3>
-                        <p>The Greatest Story Ever Folded</p>
-                        <p class="meta-info">Rank: Salvage, 2011-2012</p>
-                    </li>
-                </ul>
-            </div>
-
-            <div class="c4 col">
-                <h2 class="title">Friends & Collaborators:</h2>
-                <ul>
-                    <li><a href="">The United States of Kyle Louis Fletcher</a></li>
-                    <li><a href="">Kyle Poff</a></li>
-                    <li><a href="">Brad Sawicki</a></li>
-                    <li><a href="">The United States of Kyle Louis Fletcher</a></li>
-                    <li><a href="">Kyle Poff</a></li>
-                    <li><a href="">Brad Sawicki</a></li>
-                    <li><a href="">The United States of Kyle Louis Fletcher</a></li>
-                    <li><a href="">Kyle Poff</a></li>
-                    <li><a href="">Brad Sawicki</a></li>
-                    <li><a href="">The United States of Kyle Louis Fletcher</a></li>
-                    <li><a href="">Kyle Poff</a></li>
-                    <li><a href="">Brad Sawicki</a></li>
-                    <li><a href="">The United States of Kyle Louis Fletcher</a></li>
-                    <li><a href="">Kyle Poff</a></li>
-                    <li><a href="">Brad Sawicki</a></li>
-                    <li><a href="">The United States of Kyle Louis Fletcher</a></li>
-                    <li><a href="">Kyle Poff</a></li>
-                    <li><a href="">Brad Sawicki</a></li>
-                    <li><a href="">The United States of Kyle Louis Fletcher</a></li>
-                    <li><a href="">Kyle Poff</a></li>
-                    <li><a href="">Brad Sawicki</a></li>
-                </ul>
-            </div>
-        
-        </div><!-- c2-c4 -->
+    <div class="col-1-4 block-found">
+        <h2 class="title">Found At:</h2>
+        <ul>
+            <li><a href="">Leo Burnett DoD</a></li>
+            <li><a href="">Instagram</a></li>
+            <li><a href="">Dribbble</a></li>
+            <li><a href="">Twitter</a></li>
+            <li><a href="">LinkedIn</a></li>
+            <li><a href="">Flickr</a></li>
+            <li><a href="">Chicago Art department</a></li>
+        </ul>
+    </div>
     
-    </div><!-- row -->
-  
-</div><!-- grid -->
+    <div class="col-1-2 block-services">
+        <h2 class="title">Services:</h2>
+        <p>Branding & Identity Systems, Stationery, Icons/Pictograms/Symbols, Illustrations Print & Editorial Collarteral, Website & Application Design, Conceptualizing, Phography Direction, Environmental/Way-Finding Signage, Editorial Illustrations, Book Covers, Packaging, Installation & Exhibition Graphics, Posters, Restaurant Signage & Menu, and Screenprinting.</p>
+    </div>	    
+    
+</div><!-- row -->
+    
+<div class="grid info-grid">
+
+    <div class="col-1-4 block-instagram">
+        <?php if($instagram_photos): ?>
+        <h2 class="title">Recent Instagrams:</h2>
+        <div class="instagram-photos">
+          <?php foreach($instagram_photos as $photo_url): ?>
+          <div class="photo"><img src="<?php print $photo_url; ?>" /></div>
+          <?php endforeach; ?>  
+        </div>
+        <?php endif; ?>
+    </div>
+
+    <div class="col-1-2 block-experience">
+        <h2 class="title">Experience:</h2>
+        <ul class="info-list">
+            <li>
+                <p class="meta-info">Designer, Sept. 2009 - Present</p>
+                <h3>Leo Burnett, Department of Design</h3>
+                <p>Contribute and/or lead creativity for multiple international brands, from concept to creation for campaigns spanning print, digital, and environmental.</p>
+            </li>
+            <li>
+                <p class="meta-info">Designer, Sept. 2009 - Present</p>
+                <h3>Leo Burnett, Department of Design</h3>
+                <p>Contribute and/or lead creativity for multiple international brands, from concept to creation for campaigns spanning print, digital, and environmental.</p>
+            </li>
+            <li>
+                <p class="meta-info">Designer, Sept. 2009 - Present</p>
+                <h3>Leo Burnett, Department of Design</h3>
+                <p>Contribute and/or lead creativity for multiple international brands, from concept to creation for campaigns spanning print, digital, and environmental.</p>
+            </li>
+            <li>
+                <p class="meta-info">Designer, Sept. 2009 - Present</p>
+                <h3>Leo Burnett, Department of Design</h3>
+                <p>Contribute and/or lead creativity for multiple international brands, from concept to creation for campaigns spanning print, digital, and environmental.</p>
+            </li>
+        </ul>
+    </div>
+
+    <div class="col-1-4 block-exhibition">
+        <h2 class="title">Exhibitions:</h2>
+        <ul class="info-list">
+            <li>
+                <h3>Estate Sale</h3>
+                <p>Curator for an exhibition featuring the work of artists Ryan Maher & Ariel Diaz.</p>
+                <p class="meta-info">Chicago Art Department, Chicago, IL - 2013</p>
+            </li>
+            <li>
+                <h3>Estate Sale</h3>
+                <p>Curator for an exhibition featuring the work of artists Ryan Maher & Ariel Diaz.</p>
+                <p class="meta-info">Chicago Art Department, Chicago, IL - 2013</p>
+            </li>
+            <li>
+                <h3>Estate Sale</h3>
+                <p>Curator for an exhibition featuring the work of artists Ryan Maher & Ariel Diaz.</p>
+                <p class="meta-info">Chicago Art Department, Chicago, IL - 2013</p>
+            </li>
+            <li>
+                <h3>Estate Sale</h3>
+                <p>Curator for an exhibition featuring the work of artists Ryan Maher & Ariel Diaz.</p>
+                <p class="meta-info">Chicago Art Department, Chicago, IL - 2013</p>
+            </li>
+            <li>
+                <h3>Estate Sale</h3>
+                <p>Curator for an exhibition featuring the work of artists Ryan Maher & Ariel Diaz.</p>
+                <p class="meta-info">Chicago Art Department, Chicago, IL - 2013</p>
+            </li>
+            <li>
+                <h3>Estate Sale</h3>
+                <p>Curator for an exhibition featuring the work of artists Ryan Maher & Ariel Diaz.</p>
+                <p class="meta-info">Chicago Art Department, Chicago, IL - 2013</p>
+            </li>
+            <li>
+                <h3>Estate Sale</h3>
+                <p>Curator for an exhibition featuring the work of artists Ryan Maher & Ariel Diaz.</p>
+                <p class="meta-info">Chicago Art Department, Chicago, IL - 2013</p>
+            </li>
+            <li>
+                <h3>Estate Sale</h3>
+                <p>Curator for an exhibition featuring the work of artists Ryan Maher & Ariel Diaz.</p>
+                <p class="meta-info">Chicago Art Department, Chicago, IL - 2013</p>
+            </li>
+            <li>
+                <h3>Estate Sale</h3>
+                <p>Curator for an exhibition featuring the work of artists Ryan Maher & Ariel Diaz.</p>
+                <p class="meta-info">Chicago Art Department, Chicago, IL - 2013</p>
+            </li>
+        </ul>
+    </div>
+    
+    <div class="col-1-4">
+        <h2 class="title">Honorable Mentions:</h2>
+        <ul class="info-list">
+            <li>
+                <h3>FPO Award</h3>
+                <p>The Greatest Story Ever Folded</p>
+                <p class="meta-info">Rank: Salvage, 2011-2012</p>
+            </li>
+            <li>
+                <h3>FPO Award</h3>
+                <p>The Greatest Story Ever Folded</p>
+                <p class="meta-info">Rank: Salvage, 2011-2012</p>
+            </li>
+            <li>
+                <h3>FPO Award</h3>
+                <p>The Greatest Story Ever Folded</p>
+                <p class="meta-info">Rank: Salvage, 2011-2012</p>
+            </li>
+            <li>
+                <h3>FPO Award</h3>
+                <p>The Greatest Story Ever Folded</p>
+                <p class="meta-info">Rank: Salvage, 2011-2012</p>
+            </li>
+            <li>
+                <h3>FPO Award</h3>
+                <p>The Greatest Story Ever Folded</p>
+                <p class="meta-info">Rank: Salvage, 2011-2012</p>
+            </li>
+        </ul>
+    </div>
+
+    <div class="col-1-4">
+        <h2 class="title">Friends & Collaborators:</h2>
+        <ul>
+            <li><a href="">The United States of Kyle Louis Fletcher</a></li>
+            <li><a href="">Kyle Poff</a></li>
+            <li><a href="">Brad Sawicki</a></li>
+            <li><a href="">The United States of Kyle Louis Fletcher</a></li>
+            <li><a href="">Kyle Poff</a></li>
+            <li><a href="">Brad Sawicki</a></li>
+            <li><a href="">The United States of Kyle Louis Fletcher</a></li>
+            <li><a href="">Kyle Poff</a></li>
+            <li><a href="">Brad Sawicki</a></li>
+            <li><a href="">The United States of Kyle Louis Fletcher</a></li>
+            <li><a href="">Kyle Poff</a></li>
+            <li><a href="">Brad Sawicki</a></li>
+            <li><a href="">The United States of Kyle Louis Fletcher</a></li>
+            <li><a href="">Kyle Poff</a></li>
+            <li><a href="">Brad Sawicki</a></li>
+            <li><a href="">The United States of Kyle Louis Fletcher</a></li>
+            <li><a href="">Kyle Poff</a></li>
+            <li><a href="">Brad Sawicki</a></li>
+            <li><a href="">The United States of Kyle Louis Fletcher</a></li>
+            <li><a href="">Kyle Poff</a></li>
+            <li><a href="">Brad Sawicki</a></li>
+        </ul>
+    </div>        
+
+</div><!-- row -->
--- sites/all/themes/jfro/templates/page.tpl.php
+++ sites/all/themes/jfro/templates/page.tpl.php
@@ -1,13 +1,12 @@
-
 	<header class="header">
-		<div class="wrapper cf">
+		<div class="wrapper">
 			<h1 class="logo"><a href="/">Jason Frohlichstein <span class="quote">pronounces his name jay sun fraw lick steen.</span></a></h1>
 			<a href="mailto:hello@jasonfrohlichstein.com" class="hello">Hello</a>
 		</div>
 	</header>
 
     <nav class="nav">
-		<div class="wrapper cf">
+		<div class="wrapper">
     	    <?php print $main_menu; ?>
     	    <div class="categories">
     	        <a href="" class="cat-toggle"><span class="cat-open">+</span><span class="cat-close">-</span></a>
@@ -18,15 +17,13 @@
 
 	<section id="main" class="main">
 		<div class="wrapper">
-
             <?php print $messages; ?>
             <?php print render($page['content']); ?>
-
 		</div>
 	</section>
 
-	<footer class="footer cf">
-		<div class="wrapper cf">
+	<footer class="footer">
+		<div class="wrapper">
 			<div class="copyright">Jason Frohlichstein &copy;<?php echo date('Y'); ?> <a href="">Web Developer</a>: Brad Sawicki</div>
 			<div class="contact-email"><a href="">hello@jasonfrohlichstein.com</a></div>
 		</div>
